352b0d446ffe0d3341edcf8ac8e1ca9e0f2181a3
pinak3748 <piankfaldu123@gmail.com>
2021-05-14 21:29:55 +0530

    last

--- vision/vision.js
+++ vision/vision.js
@@ -1,8 +1,26 @@
-$(document).ready(function () {
+window.onload = function(){
+  if(!navigator.userAgent.match('Macintosh')){
+      var element = document.querySelectorAll('.slimScroll');
 
-    $('.second-button').on('click', function () {
-  
-      $('.animated-icon2').toggleClass('open');
-    });
-   
-  });
+
+      // Apply slim scroll plugin
+      var one = new slimScroll(element[0], {
+          'wrapperClass': 'scroll-wrapper unselectable mac',
+          'scrollBarContainerClass': 'scrollBarContainer',
+          'scrollBarContainerSpecialClass': 'animate',
+          'scrollBarClass': 'scroll',
+          'keepFocus': true
+      });
+
+      window.onresize = function(){
+        one.resetValues();
+        two.resetValues();
+        three.resetValues();
+    }
+}
+else{
+    document.write("For Mac users, this custom slimscroll styles will not be applied");
+}
+}
+
+
